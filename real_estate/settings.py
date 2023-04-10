c63f3c3f399a327ed7cbc813eb8725ffca7b9aed
robert5313 <kiptoorobert@zetech.ac.ke>
2023-04-10 10:47:57 +0300

    Additional files for deployment

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-yci_0y5ll-8@(c-$q)a91fji2vjbvq_d#rhsmlej5mvujsvax('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
